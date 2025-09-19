dd2471ead79eee9f00c4dd040fc968ec8911fb92
Anmol-1109 <anmolvats11.09@gmail.com>
2025-09-20 00:34:33 +0530

    data types

---
+++ data_types.cpp
@@ -0,0 +1,24 @@
+# include <iostream>
+using namespace std;
+int main(){
+int a = 94;
+int b = a*2;
+cout<<b<<endl;
+char c = 'h';
+cout<<c<<endl;
+bool bl = true;
+cout <<bl<<endl;
+double g=1.23;
+cout<<g<<endl;
+float f= 1.2;
+cout<<f<<endl;
+int size = sizeof(g);
+cout<< size<<"\n";
+
+
+//type casting//
+char ch =98;
+cout << ch<<endl;
+
+
+}
