0933e71734ae35959dbf363768f1d8f9e7ee2c16
Anmol-1109 <anmolvats11.09@gmail.com>
2025-09-25 11:34:14 +0530

    pattern3

--- while_loop.cpp
+++ while_loop.cpp
@@ -10,8 +10,12 @@ while(i<10){
      i++;
 }
 cout<<sum;}*/
+
+
+
+
 //       patern 1 //
-int n;
+/*int n;
 int m;
 cout<<"enter rows";
 cin>>n;
@@ -26,4 +30,27 @@ while(i<=n){
     }
     cout<<"\n";
     i=i+1;
+}*/
+
+
+
+//                 patern 2//
+/*int rows=4;
+int columns=4;
+for(int i=1;i<=rows;i++){
+    for(int j=1;j<=columns;j++){
+        cout<<j;
+    }cout<<"\n";
+}*/
+
+
+
+//pattern 3
+int rows=3;
+int columns=3;
+for(int j=1;j<=9;j++){
+        cout<<j;
+    if(j%3==0){
+    cout<<"\n";}
+
 }}
