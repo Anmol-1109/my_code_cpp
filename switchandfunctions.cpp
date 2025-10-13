98bc954a9dbe0c56021a8903e395f6f764ac8f55
Anmol-1109 <anmolvats11.09@gmail.com>
2025-10-14 01:08:13 +0530

    switch

---
+++ switchandfunctions.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter the no ";
+    cin>>n;
+    switch(n){
+    case 1 :
+        cout<<"1";
+    
+        break;
+     
+    case 2:
+        cout<<"2";
+        break;
+    default:
+    cout<<"enter a valid no";
+
+}
+}
