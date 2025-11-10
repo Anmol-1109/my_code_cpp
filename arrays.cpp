62a1a914c6be8fd7767712c696e8f4060155eb51
Anmol-1109 <anmolvats11.09@gmail.com>
2025-11-10 19:10:15 +0530

    corrections

--- arrays.cpp
+++ arrays.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 using namespace std;
-void print(int array[]){
-    int size;
-   size = sizeof(array)/sizeof(array[0]);
+void print( int array[],int size){
      for (int i=0;i <size;i++){
     cout<<array[i]<<endl;}
 }
 
 int main(){
+    int array2[4]={1,5,6,7};
     int array[]={0,1,2,3,4,5,6,7,8,9};
     array[5]=98;
-   // cout<<array[8]<<endl; 
-    print(array);
-}
+   // cout<<array[8]<<endl;
+   int size = sizeof(array)/sizeof(array[0]);
+    //print(array,size);
+    print(array2,4);
+} 
