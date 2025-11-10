ef1c6e1ff5670e7ad777db82b4b28e41f7269668
Anmol-1109 <anmolvats11.09@gmail.com>
2025-11-10 18:39:34 +0530

    change

--- arrays.cpp
+++ arrays.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 using namespace std;
+void print(int array[]){
+    int size;
+   size = sizeof(array)/sizeof(array[0]);
+     for (int i=0;i <size;i++){
+    cout<<array[i]<<endl;}
+}
+
 int main(){
     int array[]={0,1,2,3,4,5,6,7,8,9};
     array[5]=98;
    // cout<<array[8]<<endl; 
-    for (int i=0;i <sizeof(array)/sizeof(array[1]);i++){
-    cout<<array[i]<<endl;}
+    print(array);
 }
