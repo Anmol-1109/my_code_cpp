a0b192e7f2d8c53478001abf41b30b60fa9d547e
Anmol-1109 <anmolvats11.09@gmail.com>
2025-11-10 19:41:40 +0530

    char array

--- arrays.cpp
+++ arrays.cpp
@@ -5,6 +5,10 @@ void print( int array[],int size){
      for (int i=0;i <size;i++){
     cout<<array[i]<<endl;}
 }
+void printc( char array[],int size){
+     for (int i=0;i <size;i++){
+    cout<<array[i]<<endl;}
+}
 
 int main(){
     int array2[4]={1,5,6,7};
@@ -12,7 +16,9 @@ int main(){
     array[5]=98;
    // cout<<array[8]<<endl;
    int size = sizeof(array)/sizeof(array[0]);
-    //print(array,size);
+    print(array,size);
     print(array2,4);
-    vector<int>a(10,6);
+    vector<int>a(10,6);//vector
+    char ch[]={'a','b','c','d','e'};
+    printc(ch,5);
 } 
