24930f5a22b30d08bb6c9a1f9ab16ab9539f3186
Anmol-1109 <anmolvats11.09@gmail.com>
2025-11-16 23:23:35 +0530

    pivot problem

--- binary_search.cpp
+++ binary_search.cpp
@@ -1,50 +1,26 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-void print(int arr[],int size){
-    for (int i=0;i<size;i++){
-        cout<<arr[i]<<" ";
+int pivot(int arr[],int size){
+int s=0;
+int e=size-1;
+int mid=s+((e-s)/2);
+while(s<=e){
+    int mid=s+((e-s)/2);
+    if(arr[mid]>=arr[0] ){
+        s=mid+1;
     }
+   else{
+        e=mid;
+    }
+    if(e==s){
+        return s;
+    }
+
 }
-int search(int arr[],int size,int key){
-int start=0;
-int end=size-1;
-int array[2]={-1,-1};
 
-while(start<=end){
-    int mid= (start+end)/2;
-    if(arr[mid]==key){
-        int k=mid;
-        while(arr[mid]==key){
-            mid--;
-        }
-        while(arr[k]==key){
-            k++;
-        }
-        array[0]=mid+1;
-        array[1]=k-1;
-        
-        print(array,2);
-        break;
-    }
-    else if(arr[mid]<key){
-        start=mid+1;
-
-    }
-    else if(arr[mid]>key){
-        end=mid-1;
-
-    }
-    
-
-
-}if(array[0]==-1){cout<<-1<<" ";
-}}
+}
 int main(){
-    int array1[]={1,3,6,7,9,11,13,14,20,21,34,34,34,34};
-    int array2[]={1,4,5,6,7,8,9,11,13,13,15,16,19,25,25};
-
-    int size1 =sizeof(array1)/sizeof(int);
-    int size2 =sizeof(array2)/sizeof(int);
-   search(array1,size1,35);
-   search(array2,size2,25);
+    int arr[]={7,8,9,1,2,3,4,5};
+    int size=sizeof(arr)/sizeof(int);
+    cout<<pivot(arr,size);
 }
