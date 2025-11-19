1a7a33053fef127147fc8a340ab946af1abc2dfd
Anmol-1109 <anmolvats11.09@gmail.com>
2025-11-20 00:39:06 +0530

    pivot search using binary

--- binary_search.cpp
+++ binary_search.cpp
@@ -4,21 +4,21 @@ int pivot(int arr[],int size){
 int s=0;
 int e=size-1;
 int mid=s+((e-s)/2);
-while(s<=e){
+while(s<e){
     int mid=s+((e-s)/2);
     if(arr[mid]>=arr[0] ){
         s=mid+1;
     }
    else{
         e=mid;
-    }
-    if(e==s){
+        }}
         return s;
-    }
+    
+
+
 
 }
 
-}
 int main(){
     int arr[]={7,8,9,1,2,3,4,5};
     int size=sizeof(arr)/sizeof(int);
