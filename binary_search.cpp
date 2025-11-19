2c34bacd6801dbc60ca6531c246cec6fc49abd2f
Anmol-1109 <anmolvats11.09@gmail.com>
2025-11-20 01:06:21 +0530

    search in pivot array

--- binary_search.cpp
+++ binary_search.cpp
@@ -1,3 +1,4 @@
+
 #include<iostream>
 using namespace std;
 int pivot(int arr[],int size){
@@ -12,12 +13,52 @@ while(s<e){
    else{
         e=mid;
         }}
+    if(e==s){
         return s;
+    }
     
-}
 
+else{
+return 0;
+}}
+int searchp(int arr[],int size,int k){
+   int s=0;
+   int e=size-1;
+   int mid=s+(e-s)/2;
+   int p=pivot(arr,size);
+   
+    if(k>=arr[p]&&(k<=arr[size-1])){
+        s=p;
+    while(s<e){
+    mid=s+(e-s)/2;
+                if(arr[mid]>=k){
+                    e=mid;
+                }
+            else{
+                s=mid+1;
+            } }
+}
+if(k>arr[0]){
+    e=p-1;
+    while(s<e){
+    mid=s+(e-s)/2;
+    if(arr[mid]>=k){
+e=mid;
+    }
+    else{
+        s=mid+1;
+    }
+
+    }
+}
+if(arr[s]==k){
+    return s;
+}
+else{
+    return -1;
+}}
 int main(){
     int arr[]={7,8,9,1,2,3,4,5};
     int size=sizeof(arr)/sizeof(int);
-    cout<<pivot(arr,size);
+    cout<<searchp(arr,size,6);
 }
