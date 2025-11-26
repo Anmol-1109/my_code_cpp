bd7cecb0e43e4ec8299865eaec7e923edc712a69
Anmol-1109 <anmolvats11.09@gmail.com>
2025-11-26 14:08:32 +0530

    aggressive cows

--- book_allocation.cpp
+++ book_allocation.cpp
@@ -123,6 +123,45 @@ int ans2(int arr[],int n,int dishes){
     }
 return ans;
 }
+//aggressive cows
+bool issol1(int arr[],int n,int m,int cows){
+int count=1;
+int last=arr[0];
+
+    for(int i=1;i<n;i++){
+        
+        if((arr[i]-last)>=m){
+            last=arr[i];
+            count++;
+        }
+        if(count==cows)break;
+    }
+    if(count<cows){
+        return 0;
+    }
+    else{
+        return 1;
+    }
+
+
+}
+int sol1(int arr[],int n,int cows){
+int s=0;
+int e=arr[n-1]-arr[0];
+int m=s+(e-s)/2;
+int ans=0;
+   while(e>=s){
+     m=s+(e-s)/2;
+     if(issol(arr,n,m,cows)){
+          ans=m;
+          s=m+1;
+    }
+     else{
+          e=m-1;
+      }
+   }
+return ans;
+}
 int main(){
 int arr1[]={1};
 int n1 = sizeof(arr1)/sizeof(int);
