8b365e99a8b885103390ea538c5c9531c970fcfd
Anmol-1109 <anmolvats11.09@gmail.com>
2025-11-29 15:51:35 +0530

    optimised bubble sort

--- sorting.cpp
+++ sorting.cpp
@@ -21,12 +21,18 @@ void selectionsort(int arr[],int n){
 
 }
 void bubblesort(int arr[],int n){
-      for(int i=0;i<n-1;i++){
-        for(int j=0;j<n-i-1;j++){
-            if(arr[j]>arr[j+1])swap(arr[j],arr[j+1]);
+    bool swapped = false;
+      for(int i=0;i<n-1;i++){//int i=1;i<n;i++
+        for(int j=0;j<n-i-1;j++){//int j=0;j<n-i;j++
+            if(arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);
+                swapped =1;
+            }
 
         }
-      }
+     if(!swapped){
+        break;
+     }}
 
 }
 int main(){
