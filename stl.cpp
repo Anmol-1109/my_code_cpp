552ed77c4944453353033a859e8bfc433553e6a3
Anmol-1109 <anmolvats11.09@gmail.com>
2026-01-20 23:37:31 +0530

    deque

--- stl.cpp
+++ stl.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <array>
 #include <vector>
+#include <deque>
 using namespace std;
 //array
 int main(){
@@ -21,44 +22,79 @@ int main(){
 
 
 //vectors
-vector<int>v;
-vector<int>a(5,1);
-cout<<"this is a  ";
-for(int i:a){
-    cout<<i<<"  ";
-}
-vector<int>vec(a);
-cout<<"\nthis is vec  ";
-for(int i:vec){
-    cout<<i<<"  ";
-}
-cout<<"\n"<<v.capacity()<<"\n";
-v.push_back(1);
-cout<<v.capacity()<<"\n";
-v.push_back(2);
-cout<<v.capacity()<<"\n";
-v.push_back(3);
-cout<<v.capacity()<<"\n";
-cout<<v.size();
-cout<<"\nelement at 2 is "<<v.at(2);
-cout<<"\nat front "<<v.front();
-cout<<"\nat back "<<v.back()<<endl;
-for(int i:v){
-    cout<<i<<"  ";
+// vector<int>v;
+// vector<int>a(5,1);
+// cout<<"this is a  ";
+// for(int i:a){
+//     cout<<i<<"  ";
+// }
+// vector<int>vec(a);
+// cout<<"\nthis is vec  ";
+// for(int i:vec){
+//     cout<<i<<"  ";
+// }
+// cout<<"\n"<<v.capacity()<<"\n";
+// v.push_back(1);
+// cout<<v.capacity()<<"\n";
+// v.push_back(2);
+// cout<<v.capacity()<<"\n";
+// v.push_back(3);
+// cout<<v.capacity()<<"\n";
+// cout<<v.size();
+// cout<<"\nelement at 2 is "<<v.at(2);
+// cout<<"\nat front "<<v.front();
+// cout<<"\nat back "<<v.back()<<endl;
+// for(int i:v){
+//     cout<<i<<"  ";
 
-}
-v.pop_back();
-cout<<"\nafter pop\n";
-for(int i:v){
-    cout<<i<<"  ";
+// }
+// v.pop_back();
+// cout<<"\nafter pop\n";
+// for(int i:v){
+//     cout<<i<<"  ";
 
-}
-cout<<"\nsize befor clear"<<v.size();
-v.clear();
-cout<<"\nsize after clear"<<v.size();
+// }
+// cout<<"\nsize befor clear"<<v.size();
+// v.clear();
+// cout<<"\nsize after clear"<<v.size();
 
 
     
+
+//doube ended que or deck or deque
+deque<int>a;
+a.push_front(2);
+a.push_back(1);
+for(int i :a){
+    cout <<i<< " ";
+
+}
+a.pop_front();
+cout<<"\nafter front pop\n";
+for(int i :a){
+    cout <<i<< " ";
+
+}
+for(int i=1;i<11;i++){
+    a.push_back(i);
+
+}
+cout<<"\nafter loop\n";
+for(int i :a){
+    cout <<i<< " ";
+
+}
+cout<<"\nis a empty ---->"<<a.empty();
+a.erase(a.begin(),a.end()-4);
+cout<<"\nafter erase\n";
+for(int i :a){
+    cout <<i<< " ";
+
+}
+
+
+
+
 
 
 }
