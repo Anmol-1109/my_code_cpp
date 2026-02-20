df2bd9338df42fcd562e8831cfbf4c69396fae8b
Anmol-1109 <anmolvats11.09@gmail.com>
2026-02-20 23:53:28 +0530

    mergesort

--- sorting.cpp
+++ sorting.cpp
@@ -1,5 +1,6 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
+#include <bits/stdc++.h>
 void print(int arr[], int n){
     for (int i=0;i<n;i++){
         cout<<arr[i]<<" ";
@@ -45,18 +46,52 @@ void insertionsort(int arr[],int n){
 
             }
             else{
-                
                 break;
             }
         }
 arr[j+1]=temp;
     }
 }
+void merge(int arr[],int low, int mid ,int high){
+    int i=low;
+    int j=mid+1;
+    vector<int>v;
+    while(i<=mid&&j<=high){
+        int temp=arr[i];
+        if(arr[i]<arr[j]){
+           v.emplace_back(arr[i++]);
+        }
+        else{
+            v.emplace_back(arr[j]);
+            j++;
+        }
+
+    }
+    while(i<=mid){
+        v.push_back(arr[i++]);
+    }
+    while(j<=high){
+        v.push_back(arr[j++]);
+    }
+    
+    for(int i:v){
+        arr[low++]=i;
+    }
+
+}
+void mergesort(int arr[],int low,int high){//devide and merge
+    if(low==high)return;
+    int mid = (low+high)/2;            
+    mergesort(arr,low,mid);
+    mergesort(arr,mid+1,high);
+    merge(arr,low,mid,high);
+
+}
 int main(){
    int  arr[]={2,6,4,5,6,87,98,5,5,0};
     int n= sizeof(arr)/sizeof(int);
     print(arr,n);
-    insertionsort(arr,n);
-    print(arr,n);
+   mergesort(arr,0,n-1);
+   print(arr,n);
 
 }
