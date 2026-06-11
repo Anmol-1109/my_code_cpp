a1b3c9017b3df5c24b12627976bbd8cbc037eae7
Anmol-1109 <anmolvats11.09@gmail.com>
2026-06-12 02:13:42 +0530

    array to linked list

--- linked_list.cpp
+++ linked_list.cpp
@@ -1,6 +1,6 @@
 # include <bits/stdc++.h>;
 using namespace std;
-struct node{
+struct node{//you may use class in place of struct it will still work the class is better to use in some places as it gives the benefits of oops
     int data;
     node* next;
     node(int data1,node* next1){
@@ -8,16 +8,41 @@ struct node{
         next=next1;
 
     }
+    node(int data2){
+        data = data2;
+        next =NULL;
+    }
 };
 int main(){
-    node a=node(3,NULL);
-    node*b=&a;
-    node*c=new node(5,b);
-    cout<<b<<endl;
-    cout<<b->data<<endl;
-    cout<<b->next;
-    cout<<c<<endl;
-    cout<<c->data<<endl;
-    cout<<c->next->data<<endl;
+    // node a=node(3,NULL);
+    // node*b=&a;
+    // node*c=new node(5,b);
+    // cout<<b<<endl;
+    // cout<<a.data<<endl;
+    // cout<<b->data<<endl;
+    // cout<<b->next<<endl;
+    // cout<<c<<endl;
+    // cout<<c->data<<endl;
+    // cout<<c->next->data<<endl;
+    // node*d=new node(6);
+    // cout<<d->next<<endl;
+
+
+
+    //creating linked list;
+    int arr[]={2,3,4,5,6,76};
+    node* start=new node(arr[0]);
+    node*last=start;
+    for (int i=1;i<sizeof(arr)/sizeof(int);i++){
+        last->next=new node(arr[i]);
+        last= last->next;
+    }
+    node* a;
+    a=start;
+    while(a!=NULL){
+        cout<<a->data<<" ";
+        a=a->next;
+    }
+
 
 }
