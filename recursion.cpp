c0795738ba0268e18a0b6b7055d93b7edef6dfa5
Anmol-1109 <anmolvats11.09@gmail.com>
2026-07-05 23:35:45 +0530

    correction

--- recursion.cpp
+++ recursion.cpp
@@ -22,11 +22,24 @@ int sum(int n){
     if(n==0)return 0;
     else return n+sum(n-1);
 }
+void subseq(int arr[],int i,vector<int>&v,int sum,int n,int size){
+    if(i==size){if(sum==n)print(v);
+        return ;}
+    sum=sum+arr[i];
+    v.push_back(arr[i]);
+    subseq(arr,i+1,v,sum,n,size);
+    sum-=arr[i];
+    v.pop_back();
+    subseq(arr,i+1,v,sum,n,size);
+
+}
 int main(){
     //cout<<fibo(6);
     //cout<<sum(3);
-    int arr[]={1,2,3};
+    int arr[]={1,2,3,5};
+    int size = sizeof(arr)/sizeof(int);
     vector<int>v;
-    printsub(arr,0,v,3);
+    //printsub(arr,0,v,3);
+    subseq(arr,0,v,0,10,size);
 
 }
